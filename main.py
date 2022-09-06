3314b2e5356d58b3c3ba3c8a9970f8cb63ded3a1
nine-u98 <danielflop98@gmail.com>
2022-09-06 07:09:07 -0500

    Practica

---
+++ main.py
@@ -0,0 +1,152 @@
+import time
+from pygame import mixer
+
+
+def data():
+    lista = [
+        # Pregunta 1
+        [
+            # 0
+            ["¿Cómo son conocidos los de la Casa de Durin?"],
+            # 1
+            {'a': "Hobbits que no pertenecen a La Comarca",
+             'b': "Enanos",
+             'c': "Elfos del bosque",
+             'd': "Grandes Águilas",
+             'result': 'b'},
+            # 2
+            ["La casa de Durin se refiere al clan de enanos que habitaban las Montañas Nubladas. La mayoría de los clanes enanos pueden rastrear a sus ancestros hasta la casa de Durin."]  # 2
+        ],
+        # Pregunta 2
+        [
+            ["¿Qué es el puente de Khazad-dûm?"],  # 0
+            {'a': "El puente sobre el río Brandivino, que delimita La Comarca.",
+             'b': "Las míticas puertas entre la Tierra Media y Valinor, la tierra de los dioses.",
+             'c': "El nombre de la espada rota antes de ser forjada otra vez como la legendaria Andúril.",
+             'd': "El puente dentro de las Grandes Puertas de Moria, donde Gandalf se enfrentó al Balrog.",
+             'result': 'd'},  # 1
+            ["La casa de Durin se refiere al clan de enanos que habitaban las Montañas Nubladas. La mayoría de los clanes enanos pueden rastrear a sus ancestros hasta la casa de Durin."]  # 2
+        ],
+        # Pregunta 3
+        [
+            ["¿Qué criaturas son los Ungoliant?"],  # 0
+            {'a': "Gigantes de montaña",
+             'b': "Uruk-hai",
+             'c': "Arañas gigantes",
+             'd': "Bestias aladas",
+             'result': 'c'},  # 1
+            ["Ungoliant era una antigua criatura con forma de araña gigante. Ungoliant era la mamá de Ella-Laraña, contra quien se enfrentaron Frodo y Samsagaz Gamyi."]  # 2
+        ],
+        # Pregunta 4
+        [
+            ["¿Cuál de estos no es el nombre de un lugar para hospedarte?"],  # 0
+            {'a': "La Antigua Casa de Huéspedes",
+             'b': "El Dragón Verde",
+             'c': "El Póney Pisador",
+             'd': "La Estrella Sureña",
+             'result': 'd'},  # 1
+            ["Estrella Sureña es una variedad de \"hierba de pipa\" cultivada en La Comarca."]  # 2
+        ],
+        # Pregunta 5
+        [
+            ["¿Quiénes participaron en la Batalla de Isengard?"],  # 0
+            {'a': "Las fuerzas de Saruman contra los Rohirrim del rey Théoden",
+             'b': "Las fuerzas de Saruman contra los Ents",
+             'c': "Los Orcos de Dol Guldur contra los Galadhrim de Lothlórien",
+             'd': "Las fuerzas del Señor Oscuro, Sauron, contra las fuerzas de Gondor",
+             'result': 'b'},  # 1
+            ["La Batalla de Isengard también es conocida como La Destrucción de Isengard, porque los Ents destrozaron todo."]  # 2
+        ],
+        # Pregunta 6
+        [
+            ["¿Cuál de estas armas no fue encontrada en la cueva del Troll?"],  # 0
+            {'a': "Orcrist, la hendedora de trasgos",
+             'b': "Glamdring, el Martillo de enemigos",
+             'c': "Dardo",
+             'd': "Angrist",
+             'result': 'd'},  # 1
+            ["Angrist era una antigua espada forjada por Telchar de Nogrod. Fue utilizada para sacar un Silmaril de la Corona de Hierro de Melkor."]  # 2
+        ],
+        # Pregunta 7
+        [
+            ["¿Quién era Eleanor Gardner?"],  # 0
+            {'a': "La hija de Samsagaz Gamyi y Rosita Coto",
+             'b': "La reina del Bosque Negro y madre de Legolas",
+             'c': "Una de los nueve humanos que recibieron un Anillo de Poder",
+             'd': "La esposa de Bardo el arquero, de Ciudad del Lago",
+             'result': 'a'},  # 1
+            ["Elanor fue uno de los doce hijos que tuvieron Samsagaz Gamyi y Rosa Coto."]  # 2
+        ],
+        # Pregunta 8
+        [
+            ["¿Qué deciden Aragorn, Imrahil, Gandalf, Éomer, Elladan y Elrohir en la Última Deliberación?"],  # 0
+            {'a': "A quién enviar a destruir el Anillo de Poder",
+             'b': "Quién lideraría el Concilio Blanco. Gandalf fue escogido, pero rechazó el puesto, entoncesSaruman asumió en su lugar.",
+             'c': "El número de soldados que enviarían a luchar contra Sauron en la Batalla del Morannon",
+             'd': "Cómo castigarían a los Uruk-hai por su levantamiento",
+             'result': 'c'},  # 1
+            ["Después de la Última Deliberación, Aragorn lideró un ejército de 6000 soldados para distraer a las fuerzas de Sauron y permitir que Frodo destruyera el Anillo Único."]  # 2
+        ],
+        # Pregunta 9
+        [
+            ["¿Qué son las heridas morgul?"],  # 0
+            {'a': "Los montículos de tierra corrompidos de los que nacen los Uruk-hai.",
+             'b': "Las heridas infligidas por los Nazgûl.",
+             'c': "Las torres de asedio usadas por los orcos durante el ataque a Minas Tirith.",
+             'd': "El nombre que recibe un elfo que ha renunciado a su inmortalidad.",
+             'result': 'b'},  # 1
+            ["Las heridas morgul son heridas infligidas por las armas encantadas de los Nazgûl, como la herida que recibió Frodo que casi lo transforma en un espectro."]  # 2
+        ],
+        # Pregunta 10
+        [
+            ["¿Cómo le dicen los elfos a los hobbits?"],  # 0
+            {'a': "Los mellon",
+             'b': "Los periannath",
+             'c': "Los onodrim",
+             'd': "Los Urulóki",
+             'result': 'b'},  # 1
+            ["La palabra en Sindarin \"perian\" significa literalmente \"mediano\". \"Periannath\" es el plural."]  # 2
+        ]
+    ]
+    return lista
+
+
+def trivia(data):
+    count, incorrectos = 0, 0
+    for i in data:
+        for e in i:
+            if isinstance(e, dict):
+                for u in e:
+                    if u != 'result':
+                        print(f"{u}) {e[u]}")
+                    else:
+                        n = input(">>> ".lower())
+                        if n == e[u]:
+                            count += 1
+                            print('Correcto')
+                        else:
+                            incorrectos += 1
+                            print("Incorrecto")
+                            time.sleep(2.4)
+            else:
+                print("".join(e))
+    print('\ncorrecto', count)
+    print('incorrecto', incorrectos)
+
+
+def banners():
+    banners = ['./resources/banner2.txt', './resources/banner.txt']
+    for i in banners:
+        with open(i) as f_obj:
+            lines = f_obj.readlines()
+            for line in lines:
+                print(line)
+            time.sleep(2.0)
+
+if __name__ == '__main__':
+    mixer.init()
+    mixer.music.load('./resources/theshire.mp3')
+    mixer.music.set_volume(1.0)
+    mixer.music.play()
+    banners()
+    trivia(data())
