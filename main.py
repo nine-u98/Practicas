d934bebcde5eb13e9dae1fd2c8b29747624b46f2
nine-u98 <danielflop98@gmail.com>
2022-09-11 08:04:50 -0500

    input validados

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ WHITE = '\033[37m'
 RESET = '\033[39m'
 
 # Coleccion de preguntas
+
+
 def data():
     lista = [
         # Pregunta 1
@@ -93,7 +95,7 @@ def data():
         [
             ["¿Qué deciden Aragorn, Imrahil, Gandalf, Éomer, Elladan y Elrohir en la Última Deliberación?"],  # 0
             {'a': "A quién enviar a destruir el Anillo de Poder",
-             'b': "Quién lideraría el Concilio Blanco. Gandalf fue escogido, pero rechazó el puesto, entoncesSaruman asumió en su lugar.",
+             'b': "Quién lideraría el Concilio Blanco. Gandalf fue escogido, pero rechazó el puesto, entonces Saruman asumió en su lugar.",
              'c': "El número de soldados que enviarían a luchar contra Sauron en la Batalla del Morannon",
              'd': "Cómo castigarían a los Uruk-hai por su levantamiento",
              'result': 'c'},  # 1
@@ -123,6 +125,8 @@ def data():
     return lista
 
 # Funcion random me retorna una lista con numeros random agregados sin repetirse
+
+
 def aleatorynumber(data):
     i, lista = 0, []
     while i < len(data):
@@ -133,35 +137,49 @@ def aleatorynumber(data):
     return lista
 
 # Funcion trivia contiene toda la logica de la trivia
+
+
 def trivia(data, nombre, aleatory):
     count, incorrectos, score = 0, 0, 0
     """ Creo un ciclo que recorra mi lista de numeros aleatorios sin repeticion"""
-    for num in aleatory: #
-        for item in data[num]: # Recorre los elementos que esten en data[i]
-            if isinstance(item, dict): # Si el elemento e es de tipo dict entonces ejecuta el bloque
+    for num in aleatory:
+        for item in data[num]:  # Recorre los elementos que esten en data[i]
+            if isinstance(item, dict):  # Si el elemento e es de tipo dict entonces ejecuta el bloque
                 # recorre el diccionario en busca de las opciones - la key result
                 for key in item:
-                    if key != 'result':# Si es diferente a result ejecuta el codigo
-                        print(f"{GREEN}{key}){RESET} {MAGENTA}{item[key]}{RESET}") # Imprime la key y la pregunta item[key]
+                    if key != 'result':  # Si es diferente a result ejecuta el codigo
+                        # Imprime la key y la pregunta item[key]
+                        print(
+                            f"{GREEN}{key}){RESET} {MAGENTA}{item[key]}{RESET}")
                     else:
                         # Evalua la respuesta
                         while True:
-                            opcion = input(">>> ".lower())
-                            
-                            if opcion == 'x': # Opcion especial
-                                if input(CYAN+"Deseas ver las respuestas [s/n] : "+RESET) == 's':
-                                    for number in range(len(data) - 1): # number al rango de (0, len(data) retorna 10 - 9 )
-                                        print(
-                                            f"{BLUE}{''.join(data[number][0])}{RESET}" + f" respuesta : {GREEN}{data[number][1]['result']}{RESET}") # Imprime la key y la pregunta uso join para convertir la lista a str
-                                    exit()
-                                else: # En el caso que escriba cualquier otra letra que no sea 's' se ejecuta el else
-                                    os.system('clear') # limpia la consola
-                                    continue
-                            elif opcion == 'exit' or opcion == 'salir': # Opcion exit
+                            opcion = input(f"{YELLOW}>>> {RESET}").lower().strip()
+
+                            if opcion == 'x':  # Opcion especial
+                                while True:  # Valide el input
+                                    inpt = input(
+                                        CYAN+"Deseas ver las respuestas [Presiona (s/n)] "+RESET).lower().strip()
+                                    if inpt:
+                                        if inpt == 's':
+                                            # number al rango de (0, len(data) retorna 10 - 9 )
+                                            for number in range(len(data) - 1):
+                                                print(
+                                                    f"{BLUE}{''.join(data[number][0])}{RESET}" + f" respuesta : {GREEN}{data[number][1]['result']}{RESET}")  # Imprime la key y la pregunta uso join para convertir la lista a str
+                                            exit()
+                                        elif inpt == 'n':
+                                            exit()
+                                        else:
+                                            print(
+                                                RED+"Valor ingresado incorrecto"+RESET)
+
+                            elif opcion == 'exit' or opcion == 'salir':  # Opcion exit
                                 exit()
                             # recorre la opcion ingresada este en las opciones establecidas en data
-                            elif opcion in list(item.keys())[:-1]: # Tambien se puede poner >> n in ['a', 'b', 'c', 'd']
-                                if opcion == item[key]: # Se compara la opcion con el item[key]
+                            # Tambien se puede poner >> n in ['a', 'b', 'c', 'd']
+                            elif opcion in list(item.keys())[:-1]:
+                                # Se compara la opcion con el item[key]
+                                if opcion == item[key]:
                                     count += 1
                                     score += 10
                                     print(GREEN+'Correcto'+RESET)
@@ -173,11 +191,12 @@ def trivia(data, nombre, aleatory):
                                     print(RED+"Incorrecto"+RESET)
                                     time.sleep(1.0)
                                     break
-                            else: # En el caso que no sea una opcion establecida en data ni en las opciones te pedira volver a ingresar ya que el bucle es while True y solo en las opciones tiene un break
+                            else:  # En el caso que no sea una opcion establecida en data ni en las opciones te pedira volver a ingresar ya que el bucle es while True y solo en las opciones tiene un break
                                 print(
                                     RED+"Valor ingresado incorrecto vuelve ingresar un valor"+RESET)
-            else: # Imprime todo lo que no sea diccionario 
-                print(f'\n{YELLOW}{"".join(item)}{RESET}') # join convierte una lista a str
+            else:  # Imprime todo lo que no sea diccionario
+                # join convierte una lista a str
+                print(f'\n{YELLOW}{"".join(item)}{RESET}')
                 time.sleep(1.0)
     # Imprime los resultado
     print(f"""
@@ -188,7 +207,20 @@ def trivia(data, nombre, aleatory):
 {BLUE}{nombre} {RESET}{WHITE}tu puntuacion es :{RESET} {YELLOW}{score}{RESET}
 """)
     # Condicional evalua si se desea volver a ejecutar la trivia
-    trivia(data, nombre, aleatory) if input(CYAN+"¿Deseas volver intentar la trivia? [s]  "+RESET)  == 's' else print(WHITE+"\nEspero que lo hayas pasado muy bien, hasta pronto."+RESET)
+    #trivia(data, nombre, aleatory) if input(CYAN+"¿Deseas volver intentar la trivia? [s]  "+RESET)  == 's' else print(WHITE+"\nEspero que lo hayas pasado muy bien, hasta pronto."+RESET)
+
+    while True:  # Valide el input
+        inpt = input(
+            CYAN+"¿Deseas volver intentar la trivia? [Presiona (s/n)] "+RESET).lower().strip()
+        if inpt:
+            if inpt.lower() == 's':
+                trivia(data, nombre, aleatory)
+            elif inpt == 'n':
+                print(WHITE+"\nEspero que lo hayas pasado muy bien, hasta pronto."+RESET)
+                exit()
+            else:
+                print(RED+"Valor ingresado incorrecto"+RESET)
+
 
 # La funcion banners recorre banner.txt, lo uso como inicio a la trivia
 def banners():
@@ -207,7 +239,17 @@ if __name__ == '__main__':
 ¿Quieres realizar este reto?
 La trivia consta de 10 preguntas
 {RESET}""")
-    if input(RED+"¿Quieres iniciar? [Presiona s] "+RESET).lower() == 's':
-        nom = input(MAGENTA + "Ingresa tu usuario : " + RESET).capitalize()
-        banners()
-        trivia(data(), nom, aleatorynumber(data()))
+    while True:
+        inpt = input(RED+"¿Quieres iniciar? [Presiona (s/n)] "+RESET).lower().strip()
+        if inpt:
+            if inpt == 's':
+                while True:
+                    nom = input(MAGENTA + "Ingresa tu usuario : " +
+                                RESET).lower().capitalize().strip()
+                    if nom :
+                        banners()
+                        trivia(data(), nom, aleatorynumber(data()))
+            elif inpt == 'n':
+                break
+            else:
+                print(RED+"Valor ingresado incorrecto"+RESET)
